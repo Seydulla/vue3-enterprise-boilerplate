eacbf2920e80bdff8044e36884bbb956108d60e7
Seyit47 <seyitbu1111@gmail.com>
2022-08-04 19:53:33 +0500

    feat: store usage is removed

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,16 +1,3 @@
-<script lang="ts" setup>
-import { useCountStore } from "@/store/count";
-import { toRefs } from "vue";
-
-const countStore = useCountStore();
-
-const { count, increment } = toRefs(countStore);
-</script>
-
 <template>
   <h1>Home</h1>
-  <span>
-    {{ count }}
-  </span>
-  <button @click="increment">Increment</button>
 </template>
