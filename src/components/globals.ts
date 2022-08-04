273593036b7b9f166894aa567cc14c8d612db1ea
Seydulla Narkulyyev <seydullanarkulyyev0@gmail.com>
2022-08-04 15:06:02 +0500

    init commit

---
+++ src/components/globals.ts
@@ -0,0 +1,1 @@
+// Globally register Base Components
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import "@/components/globals"
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
