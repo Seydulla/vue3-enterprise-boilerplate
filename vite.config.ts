02ab18174a249541394ddab1795573d4bf714ebf
Seydulla Narkulyyev <seydullanarkulyyev0@gmail.com>
2022-08-05 11:15:22 +0500

    feat: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,17 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
+      "@store": fileURLToPath(new URL("./src/store", import.meta.url)),
+      "@composables": fileURLToPath(
+        new URL("./src/composables", import.meta.url)
+      ),
+      "@types": fileURLToPath(new URL("./src/types", import.meta.url)),
     },
   },
 });
